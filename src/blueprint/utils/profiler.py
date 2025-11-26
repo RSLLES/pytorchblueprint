69d86f6a2a3df7d8a2ab71d6d82c091733952994
Romain Seailles <romain.seailles@inria.fr>
2025-11-26 17:05:08 +0100

    [add] timestamp to profiler output

--- src/blueprint/utils/profiler.py
+++ src/blueprint/utils/profiler.py
@@ -3,6 +3,8 @@
 
 """Utility functions to profile models."""
 
+from datetime import datetime
+
 import torch
 
 
@@ -14,7 +16,8 @@ class MemoryProfiler:
 
     def __init__(self, prefix="memory", disable=False, exit_on_done=True):
         """Set up profiler with a prefix and options."""
-        self.prefix = prefix
+        timestamp = datetime.now().strftime("%FT%X")
+        self.filename = f"{prefix}_{timestamp}.pickle"
         self.disable = disable
         self.exit_on_done = exit_on_done
 
@@ -28,7 +31,7 @@ class MemoryProfiler:
         """Dump snapshot and stop recording; exit program if requested."""
         if self.disable:
             return
-        torch.cuda.memory._dump_snapshot(f"{self.prefix}.pickle")
+        torch.cuda.memory._dump_snapshot(self.filename)
         torch.cuda.memory._record_memory_history(enabled=None)
         if self.exit_on_done:
             exit()
